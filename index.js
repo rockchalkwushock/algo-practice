4bb432275e0f118d6b7fe271399a1d70457f570b
Cody Brunner <rockchalkwushock@icloud.com>
2018-04-28 09:10:12 -0500

    bubbleSort

--- index.js
+++ index.js
@@ -290,3 +290,18 @@ function sieveOfEratosthenes(n) {
 }
 
 sieveOfEratosthenes(50)
+
+function bubbleSort(arr) {
+  for (var i = arr.length; i > 0; i--) {
+    for (var j = 0; j < i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        var temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
+
+bubbleSort([5, 3, 8, 2, 1, 4])
