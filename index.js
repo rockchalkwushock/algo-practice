3669093bb2a045ff3a2ac55f13342383a6d54852
Cody Brunner <rockchalkwushock@icloud.com>
2018-04-28 07:51:09 -0500

    reverseArrayInPlace

--- index.js
+++ index.js
@@ -124,3 +124,15 @@ function reverseWords(str) {
 }
 
 reverseWords('this is a string of words')
+
+// can't use array.reverse
+function reverseArrayInPlace(arr) {
+  for (var i = 0; i < arr.length / 2; i++) {
+    var tempVar = arr[i]
+    arr[i] = arr[arr.length - 1 - i]
+    arr[arr.length - 1 - i] = tempVar
+  }
+  return arr
+}
+
+reverseArrayInPlace([1, 2, 3, 4, 5])
