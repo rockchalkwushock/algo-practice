35eda28e929070c2a3665b9d4f4263a94603fafa
Cody Brunner <rockchalkwushock@icloud.com>
2018-04-28 06:43:33 -0500

    harmlessRansomNote

--- index.js
+++ index.js
@@ -13,3 +13,34 @@ function fizzBuzz(num) {
 }
 
 fizzBuzz(20)
+
+// Has a linear time complexity since loops are not nested.
+// O(n + m)
+function harmlessRansomNote(noteText, magazineText) {
+  var noteArr = noteText.split(' ')
+  var magazineArr = magazineText.split(' ')
+  var magazineObj = {}
+
+  // O(m)
+  magazineArr.forEach(word => {
+    if (!magazineObj[word]) magazineObj[word] = 0
+    magazineObj[word]++
+  })
+
+  var noteIsPossible = true
+  // O(n)
+  noteArr.forEach(word => {
+    if (magazineObj[word]) {
+      magazineObj[word]--
+      if (magazineObj[word] < 0) noteIsPossible = false
+    } else {
+      noteIsPossible = false
+    }
+  })
+  return noteIsPossible
+}
+
+harmlessRansomNote(
+  'this is a secret note for you from a secret admirer',
+  'puerto rico is a place of great wonder and excitement it has many secret waterfall locations that i am an admirer of ou must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip I have for you is to go early in the morning when it is not so hot out also note that you mst wear hiking boots this is one of the best places I have ever visited'
+)
